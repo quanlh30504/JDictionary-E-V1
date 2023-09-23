868db41e240d31e9e00e58dfed1b397620f8b604
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-09-23 12:15:14 +0700

    Icon update

--- src/main/java/com/example/jdictionaryev1/HelloApplication.java
+++ src/main/java/com/example/jdictionaryev1/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.jdictionaryev1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("JDictionary-E-V");
+        stage.setTitle("JDictionary English-Vietnamese");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("https://cdn-icons-png.flaticon.com/512/5027/5027435.png"));
         stage.show();
     }
 
