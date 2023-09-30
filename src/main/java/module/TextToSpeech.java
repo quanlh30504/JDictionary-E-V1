ef323faa2811163b9f66a6fece34370e0709655c
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-10-01 00:59:11 +0700

    Update jsapi.jar

--- src/main/java/module/TextToSpeech.java
+++ src/main/java/module/TextToSpeech.java
@@ -4,17 +4,20 @@ import javax.speech.*;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class TextToSpeech {
 
     public static void main(String[] args) throws EngineException, AudioException, EngineStateError, InterruptedException {
         try {
+            Scanner scanner = new Scanner(System.in);
+            String inputText = scanner.nextLine();
             System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
             Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
             Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
             synthesizer.allocate();
             synthesizer.resume();
-            synthesizer.speakPlainText("Hello", null);
+            synthesizer.speakPlainText(inputText, null);
             synthesizer.waitEngineState(synthesizer.QUEUE_EMPTY);
             synthesizer.deallocate();
         } catch (Exception e) {
