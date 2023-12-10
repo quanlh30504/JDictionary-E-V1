465b3662ea491d2058c1ce4b183b4acd4dd64ddb
dhht0810 <tungbeo245@gmail.com>
2023-12-10 17:06:23 +0700

    - get Q&A of Round2 from database - add another set of questions for round2.Each time the game will pick random one of 2 - Clean code for some class

--- src/main/java/module/SQLite/OlympiaDB.java
+++ src/main/java/module/SQLite/OlympiaDB.java
@@ -1,10 +1,18 @@
 package module.SQLite;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class OlympiaDB extends SQLiteConnection {
     public static SQLiteConnection sqLiteConnection1 = new SQLiteConnection();
-
+    public static int getRandomIdR2() {
+        Random rand = new Random();
+        int ans = rand.nextInt(1, 3);
+        return ans;
+    }
+    public static int randomIdR2 = getRandomIdR2();
     public void setConnectionOlympia() {
         sqLiteConnection1.setConnection("jdbc:sqlite:src/main/resources/database/Olympia.db");
     }
@@ -31,13 +39,45 @@ public class OlympiaDB extends SQLiteConnection {
         return preparedStatement.executeQuery();
     }
 
-    public ResultSet addImageR2() throws SQLException {
+    public static ResultSet addImageR2() throws SQLException {
         String dataTable = "VCNV";
-        String searchQuery = "SELECT Image FROM " + dataTable;
+        String searchQuery = "SELECT Image FROM " + dataTable + " WHERE ID = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(searchQuery);
+        preparedStatement.setInt(1,randomIdR2);
         return preparedStatement.executeQuery();
     }
 
+    public static ArrayList<String> getQuestionR2() throws SQLException {
+        String dataTable = "VCNV";
+        String searchQuery = "SELECT Ques1,Ques2,Ques3,Ques4,Ques5 FROM " + dataTable + " WHERE ID = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(searchQuery);
+        preparedStatement.setInt(1,randomIdR2);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        ArrayList<String> ans = new ArrayList<>();
+        if (resultSet.next()) {
+            for (int i = 1;i<=5;i++) {
+                ans.add(resultSet.getString("Ques" + i));
+            }
+            ans.add("The vocabulary we want to talk about here is?");
+        }
+        return ans;
+    }
+
+    public static ArrayList<String> getAnswerR2() throws SQLException {
+        String dataTable = "VCNV";
+        String searchQuery = "SELECT Ans1,Ans2,Ans3,Ans4,Ans5,AnsFinal FROM " + dataTable + " WHERE ID = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(searchQuery);
+        preparedStatement.setInt(1,randomIdR2);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        ArrayList<String> ans = new ArrayList<>();
+        if (resultSet.next()) {
+            for (int i = 1;i<=5;i++) {
+                ans.add(resultSet.getString("Ans" + i));
+            }
+            ans.add(resultSet.getString("AnsFinal"));
+        }
+        return ans;
+    }
     public ResultSet executeSpeedUp() throws SQLException {
         String dataTable = "TT";
         String searchQuery = "SELECT Question FROM " + dataTable;
